eba73888618ae5ea12c1769046296292254a81aa
Ilkin-Khaniyev <xaniyevilkin.s@gmail.com>
2022-10-28 11:36:31 +0400

    created slider,navbar,announce components

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://mss-p-028-delivery.stylelabs.cloud/api/public/content/f335091cff594d608cb697e898abae38?v=fcbb785c",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://mss-p-028-delivery.stylelabs.cloud/api/public/content/f335091cff594d608cb697e898abae38?v=fcbb785c",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://mss-p-028-delivery.stylelabs.cloud/api/public/content/f335091cff594d608cb697e898abae38?v=fcbb785c",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];

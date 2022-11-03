bd22570cda5a9e4786d90e8ac532ac237d16a9a6
Ilkin-Khaniyev <xaniyevilkin.s@gmail.com>
2022-11-03 12:24:56 +0400

    login,register,product pages were created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Product from "./pages/Product";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <Home />;
+  return <Login />;
 }
 
 export default App;
